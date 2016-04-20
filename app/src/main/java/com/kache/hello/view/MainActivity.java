61856448bfe3d685f64546c811357f9d7d227960
Kajsa <kajsa.berggren@hotmail.se>
2016-04-20 15:29:34 +0200

    view

--- app/src/main/java/com/kache/hello/view/MainActivity.java
+++ app/src/main/java/com/kache/hello/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kache.hello;
+package com.kache.hello.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kache.hello.R;
 
 public class MainActivity extends AppCompatActivity {
 
