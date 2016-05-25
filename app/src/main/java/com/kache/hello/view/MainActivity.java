d0211279435b4bc2f0959e94085adf4d9866fc7e
miixell <michelle.rosengren@gmail.com>
2016-05-25 12:57:00 +0200

    Hejsan hoppsan

--- app/src/main/java/com/kache/hello/view/MainActivity.java
+++ app/src/main/java/com/kache/hello/view/MainActivity.java
@@ -11,7 +11,7 @@ import com.kache.hello.R;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton helpButton;
-
+    ImageButton gameButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,19 +20,31 @@ public class MainActivity extends AppCompatActivity {
 
         // Locate the button in activity_main.xml
         helpButton = (ImageButton) findViewById(R.id.helpButton);
+        gameButton = (ImageButton) findViewById(R.id.gameButton);
 
         // Capture button clicks
         helpButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
+                switch (arg0.getId()) {
+                    case R.id.helpButton: {
+                        // Start NewActivity.class
+                        Intent myIntent = new Intent(MainActivity.this,
+                                HelpActivity.class);
+                        startActivity(myIntent);
+                        break;
+                    }
 
-                // Start NewActivity.class
-                Intent myIntent = new Intent(MainActivity.this,
-                        HelpActivity.class);
-                startActivity(myIntent);
+                    case R.id.gameButton: {
+                        // do something for button 2 click
+                        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(intent);
+                        break;
+
+
+
+                }
+
             }
-        });
-
-
-
-    }
+        }});
 }
+    }
