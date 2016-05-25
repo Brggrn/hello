34d5a1301c007754548531672d33986d5fb60b63
Kajsa <kajsa.berggren@hotmail.se>
2016-05-25 12:43:41 +0200

    help bilder

--- app/src/main/java/com/kache/hello/view/MainActivity.java
+++ app/src/main/java/com/kache/hello/view/MainActivity.java
@@ -1,15 +1,38 @@
 package com.kache.hello.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.kache.hello.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        helpButton = (ImageButton) findViewById(R.id.helpButton);
+
+        // Capture button clicks
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        HelpActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+
     }
 }
